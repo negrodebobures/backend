5ec7e500775bb19bfbba82d6273d7edc3e43e9d4
chequeaditos <34919735+negrodebobures@users.noreply.github.com>
2020-10-08 15:05:33 -0400

    Update index.js

--- build/index.js
+++ build/index.js
@@ -75,7 +75,7 @@ typeorm_1.createConnection(DBConfig_1.dbConfig)
                 app.use(express_1.urlencoded({ extended: true }));
                 app.use(cookie_parser_1.default());
                 app.use(morgan_1.default("dev"));
-                app.use(cors_1.default({ origin: [process.env.ORIGIN, "http://localhost:3000"], credentials: true }));
+                app.use(cors_1.default({ origin: [process.env.ORIGIN, "http://localhost:3000", "http://192.168.1.101:3000"], credentials: true }));
                 // Routes
                 app.use("/api", resource_routes_1.default);
                 app.use("/api", user_routes_1.default);
